27b4004742b0f15dbb2d67b3ea5356da447ddaaa
masonsteeger94 <masonsteeger@gmail.com>
2021-02-04 22:25:31 -0600

    styled for mobile(ish)

--- portfolio/src/containers/Resume/Resume.js
+++ portfolio/src/containers/Resume/Resume.js
@@ -32,20 +32,36 @@ const Resume = (props) => {
 
     console.log(window.innerWidth)
 
-    return (
-    <div className='resume-container'>
-        <h1 style={{fontSize: "66px", marginTop: "0"}}>RESUME</h1>
-        <div className="resume" >
-        <iframe src={`${MyResume}`} title='Resume' width="600px" height="800px"></iframe>
-        </div>
-        <h2>CERTIFICATES</h2>
-        <div className="certificates">
-            <Document onClick={() => openModal(GenAss, null, 800)} className="cert" file={GenAss}><Page height={446} pageNumber={1} /></Document>
-            <Document onClick={() => openModal(ReactCert, 800, null)} className="cert" file={ReactCert}><Page width={600} pageNumber={1} /></Document>
-        </div>
-        <Modal clicked={() => closeModal()} display={modalDisplay}><Document className="modal" file={cert.file}><Page width={cert.width} height={cert.height} pageNumber={1} /></Document></Modal>
-    </div>
-    );
+    if(window.innerWidth > 768){
+        return (
+            <div className='resume-container'>
+                <h1 style={{fontSize: "66px", marginTop: "0"}}>RESUME</h1>
+                <div className="resume" >
+                    <iframe src={`${MyResume}`} title='Resume' width="690px" height="940px"></iframe>
+                </div>
+                <h2>CERTIFICATES</h2>
+                <div className="certificates">
+                    <Document onClick={() => openModal(GenAss, null, 800)} className="cert" file={GenAss}><Page height={446} pageNumber={1} /></Document>
+                    <Document onClick={() => openModal(ReactCert, 800, null)} className="cert" file={ReactCert}><Page width={600} pageNumber={1} /></Document>
+                </div>
+                <Modal clicked={() => closeModal()} display={modalDisplay}><Document className="modal" file={cert.file}><Page width={cert.width} height={cert.height} pageNumber={1} /></Document></Modal>
+            </div>
+            );
+    }else{
+        return (
+            <div className='resume-container'>
+                <h1 style={{fontSize: "66px", marginTop: "0"}}>RESUME</h1>
+                <div >
+                    <Document file={MyResume} ><Page width={320} pageNumber={1}/></Document>
+                </div>
+                <h2>CERTIFICATES</h2>
+                <div className="certificates">
+                    <Document className="cert" file={GenAss}><Page width={320} pageNumber={1} /></Document>
+                    <Document className="cert" file={ReactCert}><Page width={320} pageNumber={1} /></Document>
+                </div>
+            </div>
+            );
+    }
 };
 
 export default Resume;
