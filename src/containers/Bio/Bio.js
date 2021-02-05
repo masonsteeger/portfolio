21dad01058589d240d3acde8396521c8bbedd680
masonsteeger94 <masonsteeger@gmail.com>
2021-02-05 14:19:06 -0600

    changed up bio

--- src/containers/Bio/Bio.js
+++ src/containers/Bio/Bio.js
@@ -8,7 +8,7 @@ const Bio = (props) => {
     return (
     <div className="bio">
         <h1>BIO</h1>
-        <p>Ever since I was a young boy I've had a passion for problem solving and computers. I would always look for fun ways to modify my video games and jailbreak my iPhone for the fun of figuring out how to make it work and experience something slightly different than what the original creators had intended. As I continued on my journey through life I found myself pursuing a degree in music education at Southern Methodist University and unfortunately was not a fan of teaching in public schools.</p>
+        <p>Ever since I was a young I've had a passion for problem solving and computers. I would always look for fun ways to modify my video games and jailbreak my iPhone for the fun of figuring out how to make it work and experience something slightly different than what the original creators had intended. As I continued on my journey through life I found myself pursuing a degree in music education at Southern Methodist University and unfortunately was not a fan of teaching in public schools.</p>
         <br />
         <p>After I had graduated I found myself working in the service industry for about a year before working at an after-school music program where I would teach bass, drums, and guitar to students for the next two and a half years. Somewhere along the way, I realized that I wanted to finally go back to one of my childhood dreams of writing computer programs. I began my study at General Assembly in August of 2020, and since then have been learning and growing in different languages in hopes to land a job building programs to bring people people joy or help with their day to day lives.</p>
         <div className="images">
